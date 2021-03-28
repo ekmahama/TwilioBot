b2d1e27cb4ac21d067f9c1f2cc3760271bf5f066
Eddie <mahamaek@gmail.com>
2021-03-27 21:10:53 -0400

    version1

---
+++ app.py
@@ -0,0 +1,140 @@
+import requests
+import os
+from flask import Flask, request
+from dateutil import tz
+from twilio.twiml.messaging_response import MessagingResponse
+
+
+# API endpoint information
+
+urls = ["https://livescore-football.p.rapidapi.com/soccer/matches-by-league",
+        "https://livescore-football.p.rapidapi.com/soccer/league-table"
+        ]
+
+querystring = [{"country_code": "england",
+                "league_code": "premier-league", "timezone_utc": "0:00"},
+               {"country_code": "england", "league_code": "premier-league"}]
+
+headers = [{
+    'x-rapidapi-key': "a211671f52msh0c9150df05ba8bbp167c03jsn6ab2a432ba9b",
+    'x-rapidapi-host': "livescore-football.p.rapidapi.com"
+}, {
+    'x-rapidapi-key': "a211671f52msh0c9150df05ba8bbp167c03jsn6ab2a432ba9b",
+    'x-rapidapi-host': "livescore-football.p.rapidapi.com"
+}]
+
+# Team List
+teamList = ['Tottenham Hotspur', 'Brighton & Hove Albion',
+            'Manchester City', 'Southampton', 'Sheffield United',
+            'Liverpool', 'Aston Villa', 'Crystal Palace', 'West Ham United',
+            'Leicester City', 'Wolverhampton Wanderers', 'Newcastle United',
+            'Manchester United', 'Burnley', 'Everton', 'Fulham',
+            'West Bromwich Albion', 'Arsenal', 'Leeds United', 'Chelsea']
+
+
+to_zone = tz.gettz('America/New_York')
+
+app = Flask(__name__)
+
+
+def getData(tDay=True):
+    if tDay:
+        response = requests.request(
+            "GET", urls[0], headers=headers[0], params=querystring[0]).json()
+        return response['data']
+    return response['data']['total']
+
+
+def getTeamList(output, queryTeam, data):
+    for team in data:
+        if team['team_name'] == queryTeam:
+            output += f"{queryTeam}\n"
+            output += f"{'-----------------------------------------'}\n"
+            output += f"Postion         : {team['rank']}\n"
+            output += f"Points          : {team['points']}\n"
+            output += f"Games Played    : {team['games_played']}\n"
+            output += f"Goal Difference : {team['goals_diff']}\n"
+            output += f"Wins            : {team['won']}\n"
+            output += f"Loses           : {team['lost']}\n"
+        break
+    return output
+
+
+def getLeagueTable(output, data):
+    output += f"{'#':>2}\t{'TEAM':<20}\t{'P':<2}\t{'GD':>3}\t{'PTS':<2}\n"
+    output = + \
+        f"{'------------------------------------------------------'}\n"
+    for i, team in enumerate(data):
+        teamList.add(team['team_name'])
+        output += f"{(i+1):>2}\t{team['team_name']:<20}\t{team['games_played']:<2}\t{team['goals_diff']:>3}\t{team['points']:<2}\n"
+    return output
+
+
+@app.route('/', method=['POST'])
+def receive_sms():
+    body = request.values.get('body', '').lower().strip()
+    resp = MessagingResponse()
+
+    output = ""
+    if body == 'today':
+        data1 = getData(tDay=True)
+        toDay = datetime.today().strftime("%Y%m%d")
+
+        for match in data1:
+            startTime = str(match['time']['start'])[:8]
+            if toDay == startTime:
+                team1Name = match['team_2']['name']
+                team2Name = match['team_1']['name']
+                team1Score = match['score']['full_time']['team_1']
+                team2Score = match['score']['full_time']['team_2']
+                status = match['status']
+
+                output += f"\n{team1Name:<22} {team1Score}    :   {team2Name:<22} {team2Score} {status}"
+
+            if not output:
+                output += "No matches happening today"
+
+    elif body.lower() in teamList:
+        data = getData(tDay=False)
+        # Get information Team
+        queryTeam = body.lower()
+
+        output = getTeamList(output, queryTeam, data)
+
+        # for team in data:
+        #     if team['team_name'] == queryTeam:
+        #         output += f"{queryTeam}\n"
+        #         output += f"{'-----------------------------------------'}\n"
+        #         output += f"Postion         : {team['rank']}\n"
+        #         output += f"Points          : {team['points']}\n"
+        #         output += f"Games Played    : {team['games_played']}\n"
+        #         output += f"Goal Difference : {team['goals_diff']}\n"
+        #         output += f"Wins            : {team['won']}\n"
+        #         output += f"Loses           : {team['lost']}\n"
+        #     break
+
+    elif body.lower() == 'table':
+        data = getData(tDay=False)
+        output = getLeagueTable(output, data)
+        # # Get complete table Informaton
+        # output += f"{'#':>2}\t{'TEAM':<20}\t{'P':<2}\t{'GD':>3}\t{'PTS':<2}\n"
+        # output = + \
+        #     f"{'------------------------------------------------------'}\n"
+        # for i, team in enumerate(data):
+        #     teamList.add(team['team_name'])
+        #     output += f"{(i+1):>2}\t{team['team_name']:<20}\t{team['games_played']:<2}\t{team['goals_diff']:>3}\t{team['points']:<2}\n"
+
+    elif body.lower = "list":
+        output = '\n'.join(teamList)
+
+    else:
+        output += f{'Sorry Not information available now'}
+
+    resp.message(output)
+    return resp
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run()
--- test.py
+++ test.py
@@ -1,19 +1,95 @@
-
 import requests
 from datetime import datetime
 
-urls = ["https://api-nba-v1.p.rapidapi.com/games/live/",
-        "https://api-nba-v1.p.rapidapi.com/games/league/standard/2020"]
+urls = ["https://livescore-football.p.rapidapi.com/soccer/matches-by-league",
+        "https://livescore-football.p.rapidapi.com/soccer/league-table"
+        ]
 
-headers = {
+querystring = [{"country_code": "england",
+                "league_code": "premier-league", "timezone_utc": "0:00"},
+               {"country_code": "england", "league_code": "premier-league"}]
+
+headers = [{
     'x-rapidapi-key': "a211671f52msh0c9150df05ba8bbp167c03jsn6ab2a432ba9b",
-    'x-rapidapi-host': "api-nba-v1.p.rapidapi.com"
-}
+    'x-rapidapi-host': "livescore-football.p.rapidapi.com"
+}, {
+    'x-rapidapi-key': "a211671f52msh0c9150df05ba8bbp167c03jsn6ab2a432ba9b",
+    'x-rapidapi-host': "livescore-football.p.rapidapi.com"
+}]
 
-response = requests.request("GET", urls[1], headers=headers).json()
+response = requests.request(
+    "GET", urls[0], headers=headers[0], params=querystring[0]).json()
 
-if int((response['api']['status']) == 200):
-    print('okay')
-    games = response['api']['games']
-    for g in games:
-        print(g['startTimeUTC'])
+teamList = ['Tottenham Hotspur', 'Brighton & Hove Albion',
+            'Manchester City', 'Southampton', 'Sheffield United',
+            'Liverpool', 'Aston Villa', 'Crystal Palace', 'West Ham United',
+            'Leicester City', 'Wolverhampton Wanderers', 'Newcastle United',
+            'Manchester United', 'Burnley', 'Everton', 'Fulham',
+            'West Bromwich Albion', 'Arsenal', 'Leeds United', 'Chelsea']
+
+status = response['status']
+# for complete team information
+# data = response['data']['total']
+
+# today and tomrow Games
+# data1 = response['data']
+
+# Get information Team
+queryTeam = 'Manchester City'
+# for team in data:
+#     if team['team_name'] == 'Manchester City':
+#         print(queryTeam)
+#         print("-----------------------------------------")
+#         print(f"Postion         : {team['rank']}")
+#         print(f"Points          : {team['points']}")
+#         print(f"Games Played    : {team['games_played']}")
+#         print(f"Goal Difference : {team['goals_diff']}")
+#         print(f"Wins            : {team['won']}")
+#         print(f"Loses           : {team['lost']}")
+#     break
+
+# for game in data:
+#     homeTeam = game['team_1']['name']
+#     awayteam = game['team_1']['name']
+#     teamName.add(homeTeam)
+#     teamName.add(homeTeam)
+# score = data['score']
+# time = data['time']
+# print(teamName)
+
+
+# Get complete table Informaton
+# print(f"{'#':>2}\t{'TEAM':<20}\t{'P':<2}\t{'GD':>3}\t{'PTS':<2}")
+# print("------------------------------------------------------")
+# for i, team in enumerate(data):
+#     teamList.add(team['team_name'])
+#     print(
+#         f"{(i+1):>2}\t{team['team_name']:<20}\t{team['games_played']:<2}\t{team['goals_diff']:>3}\t{team['points']:<2}")
+# print(teamList)
+
+def getData(tDay=True):
+    if tDay:
+        response = requests.request(
+            "GET", urls[0], headers=headers[0], params=querystring[0]).json()
+        return response['data']
+    return response['data']['total']
+
+
+data1 = getData(tDay=True)
+
+# Get todays matches
+today = datetime.today().strftime("%Y%m%d")
+today = '20210306'
+
+
+for match in data1:
+    startTime = str(match['time']['start'])[:8]
+    if today == startTime:
+        team1Name = match['team_2']['name']
+        team2Name = match['team_1']['name']
+        team1Score = match['score']['full_time']['team_1']
+        team2Score = match['score']['full_time']['team_2']
+        status = match['status']
+
+        print(
+            f"{team1Name:<22} {team1Score}    :   {team2Name:<22} {team2Score} {status}")
